2784dfb809c592fd49056759f01fc4f787b31ef4
swapna.yenne@gmail.com <swapna513>
2018-11-22 21:41:42 +0000

    removed unnecessary code

--- app/src/main/java/com/ms/kotlinmvvmsample/data/source/local/LocalWeather.kt
+++ app/src/main/java/com/ms/kotlinmvvmsample/data/source/local/LocalWeather.kt
@@ -5,12 +5,6 @@ import android.arch.persistence.room.Embedded
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
 
-/**
- *
- * @author Majid Mohammadnejad
- * @version 1.0
- * @since 4/16/18
- */
 
 @Entity
 data class LocalWeather(
